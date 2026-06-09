770437d35b51d66db2314df65837e2792e67a81e
Gino <ginolencina@gmail.com>
2026-06-09 02:39:10 -0300

    test(jest): add coverage config with 50% thresholds
    - Scope coverage to src/services/**/*.js
    - Set coverage directory to 'coverage'
    - Add 50% threshold for branches, functions, lines, statements

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,14 @@
 module.exports = {
   testEnvironment: "node",
   testMatch: ["**/src/**/*.test.js"],
+  collectCoverageFrom: ["src/services/**/*.js"],
+  coverageDirectory: "coverage",
+  coverageThreshold: {
+    global: {
+      branches: 50,
+      functions: 50,
+      lines: 50,
+      statements: 50,
+    },
+  },
 };
