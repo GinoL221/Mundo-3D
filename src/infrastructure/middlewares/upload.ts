aca2d5c0949cdc8843c7bc2dfb44e7031f1d1a8c
Gino <ginolencina@gmail.com>
2026-06-19 15:41:25 -0300

    fix(upload): automatically create destination directory if it does not exist

--- src/infrastructure/middlewares/upload.ts
+++ src/infrastructure/middlewares/upload.ts
@@ -1,11 +1,16 @@
 import multer from 'multer';
 import path from 'path';
+import fs from 'fs';
 import { v4 as uuidv4 } from 'uuid';
 
 export default function createUpload(dest: string): any {
   const storage = multer.diskStorage({
     destination: (req: any, file: any, callback: any) => {
-      callback(null, path.join(process.cwd(), 'public', 'img', dest));
+      const uploadPath = path.join(process.cwd(), 'public', 'img', dest);
+      if (!fs.existsSync(uploadPath)) {
+        fs.mkdirSync(uploadPath, { recursive: true });
+      }
+      callback(null, uploadPath);
     },
     filename: (req: any, file: any, callback: any) => {
       callback(null, `${uuidv4()}${path.extname(file.originalname)}`);
